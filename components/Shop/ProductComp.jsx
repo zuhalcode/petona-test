e68263094f58c051353c9e6f18fb04a641580d13
zuhalcode <77770731+zuhalcode@users.noreply.github.com>
2022-04-23 12:57:23 +0700

    Update ProductComp.jsx

--- components/Shop/ProductComp.jsx
+++ components/Shop/ProductComp.jsx
@@ -1,10 +1,10 @@
-// import Image from "next/image";
+ import Image from "next/image";
 
 const ProductComp = ({ img, name, price, min }) => {
   return (
     <div className={`mt-2 flex max-h-max max-w-[200px] cursor-pointer flex-col border border-slate-500 bg-white shadow`}>
       <div className=" mx-auto flex w-full items-center justify-center bg-slate-200">
-        <img src={img} alt={name} className="h-[200px] w-[300px] duration-500" />
+        <Image src={img} width={300} height={200} alt={name} className="h-[200px] w-[300px] duration-500" />
       </div>
       <div className="max-w-1/2 mx-auto mt-3 bg-white text-center font-[Poppins]">
         <p className="text-center text-xl font-bold">{name}</p>
